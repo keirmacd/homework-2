6ba5327aa2c78287f000095beb752c9ded6abeb6
Keir Macdonald <keirmacd@hotmail.co.uk>
2017-01-10 15:14:20 +0000

    added two functions

---
+++ homework2.rb
@@ -0,0 +1,11 @@
+def add (a, b)
+  return a + b
+end
+
+puts add(2, 3)
+
+def population_density (population, area)
+return population / area
+end
+
+puts population_density(4872000, 51100)
--- next.rb
+++ next.rb
@@ -1,2 +1,17 @@
-def next()
+def next_number(number)
+  return number + 1
 end
+
+puts next_number(3)
+
+def length_of_text(word)
+  return word.length
+end
+
+puts length_of_text("cats")
+
+def mulitply(number1, number2)
+  return number2*number1
+end
+
+puts mulitply(2,1)
