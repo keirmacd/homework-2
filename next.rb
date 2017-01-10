a8ac5e54873bfcea4fedbe52342f4eb419350b98
Keir Macdonald <keirmacd@hotmail.co.uk>
2017-01-10 15:38:58 +0000

    added two functions

--- next.rb
+++ next.rb
@@ -15,3 +15,19 @@ def mulitply(number1, number2)
 end
 
 puts mulitply(2,1)
+
+def length_of_name(first,second)
+  return first.length + second.length
+end
+puts length_of_name("Keir","Macd")
+
+def bank_balance(pin_code)
+if pin_code == 1234
+  puts "balance 999.99"
+else
+  puts "incorrect pin"
+end
+end
+puts "enter your pin"
+pin_code= gets.chomp.to_i
+bank_balance(pin_code)
